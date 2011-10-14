f20e1acf6d9ad333436042f89aa54623f6c03b68
Matthew Sorensen <scholl.ma@gmail.com>
2011-10-13 22:47:45 -0700

    I thought that wasn't horrid enough.

--- horrid.c
+++ horrid.c
@@ -1,16 +1,10 @@
 char p,q,r,w[151];
-
 int main(int c,char**v){
-short int i=151;
-r=2^c?0:atoi(v[1]);
-while(--i>0)
-  w[i]=i==75;
- while(i++<40){
-   for(p=c=0;c<150;){
-     q=w[c];
-     w[c]=r&(p?240:15)&(w[c]?204:51)&(w[++c]?170:85);
-     putchar((p=q)?35:32);
-   }
-   puts("");
- }
+  short int i=151;
+  r=2^c?0:atoi(v[1]);
+  while(--i>0)
+    w[i]=i==75;
+  while(i++<40 & puts(""))
+    for(p=c=0;c<150;putchar(32+((p=q)&&1)))                           
+      w[c]=r&(p?240:15)&((q=w[c])?204:51)&(w[++c]?170:85);
 }
